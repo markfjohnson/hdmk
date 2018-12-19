909f6308a681d5b943cdc7400af45932fe6be2dc
markjohnson <markfjohnson@gmail.com>
2018-12-19 01:22:03 -0500

    Added the Kafka consumer K8s service

--- kafka_hello_world/kafka_consumer.py
+++ kafka_hello_world/kafka_consumer.py
@@ -1,33 +1,24 @@
 # Before running this program run pip install kafka-python
+from __future__ import print_function
 from kafka import KafkaConsumer
 from json import loads
-import sys, getopt
 
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
-broker = ''
+import os, sys
 
-try:
-    opts, args = getopt.getopt(sys.argv, "hi:", ["broker="])
-except getopt.GetoptError:
-    print('test.py -i <inputfile> -o <outputfile>')
-    sys.exit(2)
 
-for opt, arg in opts:
-    if opt == '-h':
-        print('test.py -i <inputfile> -o <outputfile>')
-        sys.exit()
-    elif opt in ("-b", "--broker"):
-        broker = arg
+print("App started X")
+
 
 consumer = KafkaConsumer(
     'demo-transactions',
-    bootstrap_servers=[broker],
-    auto_offset_reset='earliest',
-    enable_auto_commit=True,
-    group_id='my-group',
-    value_deserializer=lambda x: loads(x.decode('utf-8')))
+    bootstrap_servers=["broker.kafka.l4lb.thisdcos.directory:9092"],
+    auto_offset_reset='earliest')
 
+consumer.subscribe(["transactions"])
+
+print("E")
 for message in consumer:
     message = message.value
     print('Message: {}'.format(message))
+
+print("End of consumer reader")
